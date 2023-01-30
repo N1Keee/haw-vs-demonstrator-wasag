40a97e95e074b94dfe8a51bf0c267770b6aa96b0
Kunstkarl <62984068+Kunstkarl@users.noreply.github.com>
2023-01-30 23:38:57 +0100

    Dolly track
    Läuft jetzt durchgehend und ohne damping
    TODO: Kreis Form für Demo mode

---
+++ Demonstrator - Akkustische Ortung/Assets/Scripts/DollyLogic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class DollyLogic : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private float moveSpeed;
+
+    void Loop()
+    {
+        virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition += (1) * (moveSpeed * (Time.deltaTime));
+        
+        if (virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition > 1)
+        {
+            virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = 0;
+        }
+    }
+    void Start()
+    {
+        Debug.Log(virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition);
+    }
+    
+    void Update()
+    {
+        Loop();
+    }
+}
