d7056e2d881f6d2c18f5db543c4a0329e5956bd2
Jonas <57606077+N1Keee@users.noreply.github.com>
2023-02-05 19:17:02 +0100

    Folder for Training-Scripts

---
+++ Demonstrator - Akkustische Ortung/Assets/Scripts/Training/HeadphoneState.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadphoneState : MonoBehaviour
+{
+    private Animator _animator;
+    private void Awake() => _animator = transform.GetComponent<Animator>();
+    
+    void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Mounted"))
+        {
+            transform.gameObject.SetActive(false);
+        }
+    }
+}
