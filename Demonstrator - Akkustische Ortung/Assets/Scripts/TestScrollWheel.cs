4467a30ab442f6216bc77306abbad3579b246a3e
Jonas <57606077+N1Keee@users.noreply.github.com>
2023-02-05 14:44:54 +0100

    Add ScrollFuntion ChannelKnob

---
+++ Demonstrator - Akkustische Ortung/Assets/Scripts/TestScrollWheel.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScrollWheel : MonoBehaviour
+{
+    private bool _scrollEnabled;
+    [SerializeField] private GameObject channelKnob;
+    [SerializeField] private float scrollSpeed;
+    private Material _defaultMaterial;
+
+    [SerializeField] private Material highlightMaterial;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _scrollEnabled = false;
+        _defaultMaterial = channelKnob.GetComponent<MeshRenderer>().material;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_scrollEnabled)
+        {
+            var speed = Input.GetAxisRaw("Mouse ScrollWheel") * Time.deltaTime;
+            channelKnob.transform.Rotate(0,0,speed * scrollSpeed);
+        }
+    }
+
+    public void ToggleScrollable()
+    {
+        _scrollEnabled = !_scrollEnabled;
+        if (_scrollEnabled)
+        {
+            channelKnob.GetComponent<MeshRenderer>().material = highlightMaterial;
+        }
+        else
+        {
+            channelKnob.GetComponent<MeshRenderer>().material = _defaultMaterial;
+        }
+    }
+}
