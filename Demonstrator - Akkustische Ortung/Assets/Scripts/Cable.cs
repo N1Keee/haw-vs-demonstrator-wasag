bebc5c3138b71d133f65c4497df38f195f7d7fb6
Jonas <57606077+N1Keee@users.noreply.github.com>
2023-02-07 14:07:32 +0100

    Add CableLogic

--- Demonstrator - Akkustische Ortung/Assets/Scripts/Cable.cs
+++ Demonstrator - Akkustische Ortung/Assets/Scripts/Cable.cs
@@ -3,31 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cable : MonoBehaviour       // can be deleted !!!
+public class Cable : MonoBehaviour
 {
-    public LineRenderer lineRenderer;
-    [SerializeField] private Transform[] positions;
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject geophone;
+    [SerializeField] private GameObject cable;
+    private Animator _animator;
+
+    private void ShowHide()
     {
-        lineRenderer.positionCount = positions.Length;
+        if(_animator.GetCurrentAnimatorStateInfo(0).IsName("Placed") || _animator.GetCurrentAnimatorStateInfo(0).IsName("Placed_demo"))
+        {
+            cable.gameObject.SetActive(true);
+        }
+        else
+        {
+            cable.gameObject.SetActive(false);
+        }
     }
     
-    public void DrawLines()
+    private void Start()
     {
-        if (!lineRenderer.gameObject.activeSelf)
-        {
-            lineRenderer.transform.gameObject.SetActive(true);
-        }
-        for (int i = 0; i < positions.Length; i++)
-        {
-            lineRenderer.SetPosition(i, positions[i].position);
-        }
+        _animator = geophone.GetComponent<Animator>();
     }
 
-    public void HideLines()
+    private void Update()
     {
-        lineRenderer.transform.gameObject.SetActive(false);
+        ShowHide();
     }
 }
